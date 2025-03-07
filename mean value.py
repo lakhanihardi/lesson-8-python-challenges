bda85796afe41b4ffe7fe4c99382240e159d5c60
lakhanihardi <hardi.jgd@gmail.com>
2025-03-07 09:35:26 +0530

    mean values

---
+++ mean value.py
@@ -0,0 +1,10 @@
+mean=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=mean*total_number
+print("the sum of 40 number:",sum)
+num2=sum-((wrong_number)-(correct_number))
+print("sum((wrong_number)-(correct_number)):",num2)
+mean=num2/total_number
+print(mean)
